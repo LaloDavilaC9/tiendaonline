b74058eaa73e5f02a234f4cd9f91b47de242a504
VictorLuna-ISC <75042129+VictorLuna-ISC@users.noreply.github.com>
2021-11-11 22:03:58 -0600

    Prueba
    Probé algo

--- login.php
+++ login.php
@@ -1,8 +1,84 @@
-<<html>
-    <head>
-        <title>Inicar Sesión</title>
-    </head>
-    <body>
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="design.css">
+    <title>OVERTHINKING LOGIN</title>
+</head>
+
+<body>
+    <?php
+        //Hacemos la conexion.
+        include("sqlConn.php");
+        $conexion = conectar();
+    ?>
+    <TABLE class="estandarTablaDesign" CELLSPACING=0 CELLPADDING=7>
+            <TR>
+                <TD> <!--Esto se realizo para dar formato y diseño a la pagina-->
+                    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    <br><br><br><br><br><br><br><br><br><br><br><br>
+                </TD>
+                <TD width=100% align="center">
+                    <TABlE>
+                        <TR>
+                            <TD class="imagenLogin"><!--Esto se realizo para dar formato y diseño a la pagina-->
+                                <br><br><br><br><br><br><br><br>
+                                <br><br><br><br><br><br><br><br>
+                                <br><br><br><br><br><br><br><br>
+                                <br><br>
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                            </TD>
+                            <TD class="formDesign">
+                                <div style="text-align: center;">
+                                    <H2>LOGIN</H2>
+                                    <HR><br><br><br><br><br></HR>
+                                    <div>
+                                        <!--Creamos el form que captura los datos de inicios sesion y los manda a-->
+                                        <!--otra pagina que valida que sean correctos los datos.-->
+                                        <form action="verificarSesion.php" method="post">
+                                            <h3><IMG SRC="Recursos/usuario.png" ALT="IMAGEN NO DISPONIBLE"> Usuario: <input class="camposDesign" type="text" name="usuario" required><br><br></h3>
+                                            <h3><IMG SRC="Recursos/contrasena.png" ALT="IMAGEN NO DISPONIBLE"> Contraseña: <input class="camposDesign" type="password" name="contrasenia" required><br><br></h3>
+                                            <input class="botonLoginDesign" type="submit" value="Iniciar sesión">
+                                        </form>
+                                    </div>
+                                    <!--Permitimos que se navegue por la pagina pero sin crear una sesion.-->
+                                    <p>¿No tienes una cuenta? <a href="paginaPrincipal.php">Continuar sin iniciar sesión</a>
+                                        <br><br><br><br><br>
+                                    </p>
+                                </div>
+                            </TD>
+                            <TD>
+                                &nbsp&nbsp
+                            </TD>
+                        </TR>
+                    </TABlE>
+                </TD>
+            </TR>
+    </TABLE>
+</body>
 </html>
