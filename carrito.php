074957ccea5a6c7cb543852f37bff4559d7be65a
lalodc9 <al269314@edu.uaa.mx>
2021-11-19 18:58:09 -0600

    Update de la base de datos
    Corregí un pequeño problema de la base de dato

--- carrito.php
+++ carrito.php
@@ -4,6 +4,12 @@
     $_SESSION['login']="";
     $_SESSION['carrito'][0] = 1;
     $_SESSION['carrito'][1] = 2;
+    $_SESSION['carrito'][2] = 3;
+    $_SESSION['carrito'][3] = 4;
+    $_SESSION['carrito'][4] = 5;
+    $_SESSION['carrito'][5] = 6;
+    $_SESSION['carrito'][6] = 7;
+    $_SESSION['carrito'][7] = 8;
 ?>
 <html>
     <head>
